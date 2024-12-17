068342f9b3c5e5efd687d11347dc455905d1565f
shihabarifi <shihabriotkd@gmail.com>
2024-12-18 01:23:44 +0300

    Add project files.

---
+++ RazorPageInvontory/Program.cs
@@ -0,0 +1,35 @@
+﻿using RazorPageInvontory.ServicesLayer;
+
+var builder = WebApplication.CreateBuilder(args);
+// إضافة خدمات Razor Pages
+builder.Services.AddRazorPages();
+
+// تسجيل الخدمات في نظام حقن التبعية
+builder.Services.AddScoped<ISalesInvoiceService, SalesInvoiceService>();
+//builder.Services.AddScoped<ISalesInvoiceRepository, SalesInvoiceRepository>();
+
+// إعداد HttpClient للتعامل مع API
+builder.Services.AddHttpClient("SalesAPI", client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["APISettings:BaseUrl"]);
+    client.DefaultRequestHeaders.Add("Accept", "application/json");
+});
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
